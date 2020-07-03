6101bc622a5540fb558613eb97b1067d8c3b476f
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-07-03 18:09:13 -0500

    Se termino diseño de una suma

--- app/src/main/java/gustavo/me/gustavocalzada/appuno/MainActivity.java
+++ app/src/main/java/gustavo/me/gustavocalzada/appuno/MainActivity.java
@@ -12,21 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int matematicas = 5;
-        int quimica =  5;
-        int fisica = 5;
-        int promedio = 0;
-
-        promedio = (matematicas + quimica + fisica) / (3);
-
-        if(promedio >= 6) {
-            Toast.makeText(this,"Alumno Aprobado", Toast.LENGTH_SHORT).show();
-        } else if(promedio <= 5) {
-            Toast.makeText(this, "Alumno Reprobado", Toast.LENGTH_SHORT).show();
-        }
-
-
-
 
 
 
